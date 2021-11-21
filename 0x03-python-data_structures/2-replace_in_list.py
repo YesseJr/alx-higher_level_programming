fd0231920af54a23a701a4bd92f571fd5dce8231
YesseJr <thonnieyesse@hotmail.com>
2021-11-21 06:59:59 +0300

    Replace it

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,6 +2,6 @@
 def element_at(my_list, idx):
     if idx < 0:
         my_list[idx] = element
-    elif idx >= len(my_list):
+    elif idx > len(my_list):
         my_list[idx] = element
     return (my_list)
