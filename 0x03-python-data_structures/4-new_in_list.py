bb379cc3253b5ebde8e74ffa6e1b0a220732e2a1
YesseJr <thonnieyesse@hotmail.com>
2021-11-21 08:11:51 +0300

    New in list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,7 +4,7 @@ def new_in_list(my_list, idx, element):
         return(my_list)
     if idx > len(my_list):
         return(my_list)
-    new_list = list(my_list)
-    new_list[idx] = element
-    return(new_list)
-    return(my_list)
+     new_list = my_list[:idx]
+    new_list.append(element)
+    new_list += my_list[idx + 1:]
+    return new_list
