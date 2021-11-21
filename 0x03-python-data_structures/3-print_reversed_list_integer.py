947e019cf227a05285ccb0f2baea4723f0c8607c
YesseJr <thonnieyesse@hotmail.com>
2021-11-21 07:36:36 +0300

    Print them in reversed fashion

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-# What does the Isinstance function do?
-# The isinstance() function returns True if the specified object is of the specified type, otherwise False
+
 def print_reversed_list_integer(my_list=[]):
     if isinstance(my_list, list):
         for i in reversed(my_list):
