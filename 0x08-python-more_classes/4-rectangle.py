3fecd3d94a933f50d8377f3af588016a6c0b89cc
YesseJr <thonnieyesse@hotmail.com>
2021-12-13 15:54:52 +0300

    Eval is magic

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -8,7 +8,12 @@ class Rectangle:
     """
 
 def __init__(self, width=0, height=0):
-        """ Initialize rectangles """
+        """
+        Initialize rectangles
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+            """
         self.width = width
         self.height = height
 
