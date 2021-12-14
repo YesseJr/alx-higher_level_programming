8181d512758518c67ea0f71dc07c93f5211896e7
YesseJr <thonnieyesse@hotmail.com>
2021-12-14 19:31:08 +0300

    Magic

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(count=[-1]):
-    count[0] += 1
-    return "BestSchool" + ", BestSchool" * count[0]
+def magic_string()
+   for i in range(n):
+       print("BestSchool")
