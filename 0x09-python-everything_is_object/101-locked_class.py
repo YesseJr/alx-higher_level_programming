f4c8dab7c9f930735c8a066307e5ad6f5bc61eea
YesseJr <thonnieyesse@hotmail.com>
2021-12-14 20:02:05 +0300

    Lock it

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,21 @@
 #!/usr/bin/python3
-class LockedClass:
-    __slots__ = 'first_name'
+"""
+Defines class with no class or object attribute
+"""
+
+
+class LockedClass():
+    """
+    Prevent user from creating new instance attribute dynamically
+    unless attribute is "first_name"
+    >>> a = LockedClass()
+    >>> a.first_name = "Waython"
+    >>> a.first_name
+    "Waython"
+    >>> a.last_name = "Yesse"
+    Traceback (most recent call last):
+    ...
+    AttributeError: 'LockedClass' object has no attribute 'last_name'
+    """
+
+    __slots__ = "first_name"
