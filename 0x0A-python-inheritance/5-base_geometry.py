0bead3cff18450d6989407a7019f763b1d5f734e
YesseJr <thonnieyesse@hotmail.com>
2021-12-20 20:19:21 +0300

    This is an empty class BaseGeometry

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
 """
-This module contains method inherits_from
-returns True if obj is instance of class that it inherits from or is subcls of
+This module defines an empty class BaseGeometry.
 """
 
 
-def inherits_from(obj, a_class):
-    """
-    Args:
-        obj: An object.
-        a_class: A class.
-    Return:
-        True if obj is instance of class that it inherits from or is subcls of
-    """
-    return (type(obj) is not a_class and issubclass(type(obj), a_class))
+class BaseGeometry:
+    """Represent base geometry."""
+    pass
