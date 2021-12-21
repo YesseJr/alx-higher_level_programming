ea1ce70cd6b6908b3ec059b02ae2a3b47d4c83de
YesseJr <thonnieyesse@hotmail.com>
2021-12-21 18:18:52 +0300

    Class Student

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
-This module defines a class Student.
+This module defines a class Student that initializes public instance attributes first_name, last_name, and age,
+and has public method to_json that returns dictionary representation
+of requested attributes or all if none were requested
 """
 
 class Student():
