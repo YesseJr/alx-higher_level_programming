0d5ebf3ae1bd8e34667aa950bd5a52e9125f871d
YesseJr <thonnieyesse@hotmail.com>
2021-12-21 18:55:11 +0300

    Triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -7,6 +7,17 @@ This module defines a Pascal's Triangle function.
 def pascal_triangle(n):
     """
     Represent Pascal's Triangle of size n.
+Return:
+        empty list [] if n <= 0
+    if n is 8, we should expect:
+            [1]
+            [1, 1]
+            [1, 2, 1]
+            [1, 3, 3, 1]
+            [1, 4, 6, 4, 1]
+            [1, 5, 10, 10, 5, 1]
+            [1, 6, 15, 20, 15, 6, 1]
+            [1, 7, 21, 35, 35, 21, 7, 1]
     """
     if n <= 0:
         return []
