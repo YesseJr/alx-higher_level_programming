6092314a08ae7df81f4036be956a74d036b0e1c1
YesseJr <thonnieyesse@hotmail.com>
2021-12-21 18:31:53 +0300

    Class Student

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -4,13 +4,18 @@ This module defines a class Student.
 """
 
 
-class Student():
-   """
-   Represent a student.
-   """
+class Student:
+    """
+    Represent a student.
+    """
+
     def __init__(self, first_name, last_name, age):
         """
-        Initializes student with full name and age
+        Initialize a new Student.
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
         """
         self.first_name = first_name
         self.last_name = last_name
@@ -18,26 +23,22 @@ class Student():
 
     def to_json(self, attrs=None):
         """
-        Returns dictionary description with simple data structure
-        (list, dictionary, dictionary, string)
-        for JSON serialization of an object
-        Return:
-            Only return dict of attrs given to us
-            Return entire dict if no attrs given
+        Get a dictionary representation of the Student.
+        If attrs is a list of strings, represents only those attributes
+        included in the list.
+        Args:
+            attrs (list): (Optional) The attributes to represent.
         """
-        if attrs is None:
-            return self.__dict__
-        else:
-            dic = {}
-            for att in attrs:
-                if att in self.__dict__.keys():
-                    dic[att] = self.__dict__[att]
-            return dic
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
 
     def reload_from_json(self, json):
         """
-        Return:
-            Transfer all attributes of json to self
+        Replace all attributes of the Student.
+        Args:
+            json (dict): The key/value pairs to replace attributes with.
         """
         for k, v in json.items():
             setattr(self, k, v)
