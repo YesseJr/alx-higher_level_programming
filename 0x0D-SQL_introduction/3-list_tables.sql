e69bdf30c53d16377837c1be993c9c76e074798c
YesseJr <thonnieyesse@hotmail.com>
2022-02-02 10:54:08 +0300

    This is a script that lists all the tables of a database in  MySQL server

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,6 @@
+-- This is a script that lists all the tables of a database in MySQL server
+-- The database name will be passed as argument of mysql command.
+-- cat [filename] | mysql -hlocalhost -uroot -p mysql
+-- Author: Waython Yesse
+
+SHOW tables;
