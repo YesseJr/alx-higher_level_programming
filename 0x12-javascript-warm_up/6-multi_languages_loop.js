506e373d53fb2d0df339b71bd10d20ff94057401
YesseJr <thonnieyesse@hotmail.com>
2022-03-01 06:24:53 +0300

    Updated

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
-const lines = [
-    'C is fun',
-    'Python is cool',
-    'JavaScript is amazing'
-  ];
-  
-  for (let i = 0; i < lines.length; i++) {
-    console.log(lines[i]);
-  }
+let texts = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < texts.length; i++) {
+  console.log(texts[i]);
+}
