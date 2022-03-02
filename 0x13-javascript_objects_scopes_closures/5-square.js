cddd954e632aaaa442f5688e1166369f1c2b4781
YesseJr <thonnieyesse@hotmail.com>
2022-03-02 06:07:01 +0300

    Square

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,37 +1,17 @@
 #!/usr/bin/node
-class Rectangle {
-    constructor (w, h) {
-      if (w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;
-      }
-    }
-  
-    print () {
-      for (let i = 0; i < this.height; i++) {
-        for (let j = 0; j < this.width; j++) { process.stdout.write('X'); }
-        console.log('');
-      }
-    }
-  
-    rotate () {
-      const a = this.width;
-      const b = this.height;
-      this.width = b;
-      this.height = a;
-    }
-  
-    double () {
-      this.width *= 2;
-      this.height *= 2;
-    }
+const Rectangle = require('./4-rectangle');
+
+/**
+ * Represents a Rectangle with 4 equal sides.
+ */
+class Square extends Rectangle {
+  /**
+   * Creates a new Square with the given dimensions.
+   * @param {Number} size The value of the width and height.
+   */
+  constructor (size) {
+    super(size, size);
   }
-  
-  class Square extends Rectangle {
-    constructor (size) {
-      super(size, size);
-    }
-  }
-  
-  module.exports = Rectangle;
-  module.exports = Square;
+}
+
+module.exports = Square;
