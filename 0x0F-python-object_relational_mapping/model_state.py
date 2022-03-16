f2fd4deaeec67ce78de692bac31d40584210f2c1
YesseJr <thonnieyesse@hotmail.com>
2022-03-16 06:53:58 +0300

    Fixed

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-"""
-Defines a State model.
-Inherits from SQLAlchemy Base and links to the MySQL table states.
-"""
+"""Module for states table"""
+from sqlalchemy import Table, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
-from SQLAlchemy import Column, Integer, String
-from SQLAlchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
 class State(Base):
-    """Represents a state for a MySQL database.
-    __tablename__ (str): The name of the MySQL table to store States.
-    id (SQLAlchemy.Integer): The state's id.
-    name (SQLAlchemy.String): The state's name.
-    """
-    __tablename__ = "states"
+    """A class for state"""
+    __tablename__ = 'states'
+
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
+
+    def __repr__(self):
+        """Object Representation"""
+        return "<States(name =' %s')>" % (self.name)
