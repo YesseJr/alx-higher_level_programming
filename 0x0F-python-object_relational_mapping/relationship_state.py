3adbf2d1d87f18795b41e98275b02c838fe86923
YesseJr <thonnieyesse@hotmail.com>
2022-03-16 07:34:06 +0300

    Simply a relationship

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+The Relationship of states
+"""
+from sqlalchemy import Table, Column, Integer, String
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """A class for state"""
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
