ae131b7e458030de85586117ca5cdf693021b201
YesseJr <thonnieyesse@hotmail.com>
2022-03-17 07:28:14 +0300

    Technical

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Defines a peak-finding algorithm
+"""
+
+
+def find_peak(list_of_integers):
+    """
+    Function that finds a peak in a list of unsorted integers
+    """
+    if list_of_integers:
+        list_of_integers.sort(reverse=True)
+        return list_of_integers[0]
