f4db56bf8c56c70476c08966777e41177438c2b4
YesseJr <thonnieyesse@hotmail.com>
2022-03-18 19:45:54 +0300

    Another one

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Takes in a URL, sends a request to the URL and displays the value of the variable X-Request-Id in the response header
+Displays X-Request-Id header variable of a request to a given URL.
 """
 from sys import argv
 import requests
