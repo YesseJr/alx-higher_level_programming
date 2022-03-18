f2f610ccd43d729b6e7fb91172e47b3f89ddd1aa
YesseJr <thonnieyesse@hotmail.com>
2022-03-18 19:51:03 +0300

    Body of response

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Takes in a URL, sends a request to the URL and displays the body of the response
+Displays the body of the response.
 """
 from sys import argv
 import requests
