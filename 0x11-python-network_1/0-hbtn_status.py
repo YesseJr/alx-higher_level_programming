06ff9697d8e80f537136ecf95cc2a17c33dde8cf
YesseJr <thonnieyesse@hotmail.com>
2022-03-18 19:13:00 +0300

    My Status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,11 +6,13 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    
-    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
-        if response.readable():
-            data = response.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(data)))
-            print("\t- content: {}".format(data))
-            print("\t- utf8 content: {}".format(data.decode("utf-8")))
+    try:
+        with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
+            if resp is not None:
+                html = resp.read()
+                print("Body response:")
+                print("\t- type: {}".format(type(html)))
+                print("\t- content: {}".format(html))
+                print("\t- utf8 content: {}".format(html.decode('utf-8')))
+    except Exception:
+        pass
