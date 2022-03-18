d99794d1ca7e41c50e76de7f5a1aa3235a73ef28
YesseJr <thonnieyesse@hotmail.com>
2022-03-18 20:07:01 +0300

    Fixed

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,8 +4,8 @@ Fetches https://alx-intranet.hbtn.io/status.
 """
 import requests
 
+
 if __name__ == "__main__":
-    
     req = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(req.text)))
