5d1e5745cb4fc751a5e2032e288795cc0cded06d
YesseJr <waythonny@yahoo.com>
2022-07-06 16:58:55 +0300

    ReadMe

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-
-fs.readFile(process.argv[2], 'utf8', (err, data) => {
-  if (err) throw err;
-  console.log(data);
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
