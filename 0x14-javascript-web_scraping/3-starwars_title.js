418a2b917616a84540f47f74e1a50ae8649bb665
YesseJr <waythonny@yahoo.com>
2022-07-06 17:23:16 +0300

    'Em all

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,22 @@
 #!/usr/bin/node
+let id = process.argv[2];
+let url = 'http://swapi.co/api/films/' + id;
 const request = require('request');
-let url = 'http://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else #!/usr/bin/node
+  const request = require('request');
+  request('http://swapi.co/api/films/' + process.argv[2], function (err, resp, body) {
+    if (err) {
+      console.log(err);
+    } else if (resp.statusCode === 200 && resp.headers['content-type'] === 'application/json') {
+      console.log(JSON.parse(body).title);
+    }
+  });if (response.statusCode === 200) {
+    body = JSON.parse(body);
+    console.log(body['title']);
+  } else {
+    console.log('Erorr Code:' + response.statusCode);
+  }
 });
